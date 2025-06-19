cd1f8cb99aa056c20884db6f3623ed9916189baf
rhit-drummk <drummk@rose-hulman.edu>
2025-06-19 10:38:42 -0500

    a few things were done

--- 01-MovingSmile/MovingSmile.py
+++ 01-MovingSmile/MovingSmile.py
@@ -7,12 +7,33 @@ def main():
     pygame.display.set_caption("Moving Smile")
     screen = pygame.display.set_mode((640, 480))
 
+    eye_x = 0
+    eye_y = 0
+    mouth_y = 0
+    clock = pygame.time.Clock()
+
     while True:
         # TODO 4: Set the clock speed to 60 fps
+
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # TODO 3: Make the eye pupils move with Up, Down, Left, and Right keys
+            keys_pressed = pygame.key.get_pressed()
+            if keys_pressed[pygame.K_LEFT]:
+                eye_x = eye_x-15
+            if keys_pressed[pygame.K_RIGHT]:
+                eye_x = eye_x+15
+
+            if keys_pressed[pygame.K_UP]:
+                eye_y = eye_y-15
+            if keys_pressed[pygame.K_DOWN]:
+                eye_y = eye_y+15
+
+            if keys_pressed[pygame.K_SPACE]:
+                mouth_y = mouth_y+10
 
         screen.fill((255, 255, 255))  # white
 
@@ -23,19 +44,21 @@ def main():
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (242+eye_x, 162+eye_y), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (398+eye_x, 162+eye_y), 7)  # black pupil
 
         # TODO 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
         # API --> pygame.draw.circle(screen, (r,g,b), (x, y), radius, thickness)
+        #pygame.draw.ellipse(screen, (225, 200, 0), (320, 215, 100, 100), 2)
 
         # TODO 2: Draw a mouth
         # Suggestion: color (0,0,0), x 230, y 320, width 180, height 30
         # API --> pygame.draw.rect(screen, (r,g,b), (x, y, width, height), thickness)
+        pygame.draw.ellipse(screen, (225, 27, 0), (220, 400-mouth_y, 200, 50))
 
         pygame.display.update()
 
--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -4,26 +4,29 @@ import sys
 
 def main():
     # pre-define RGB colors for Pygame
-    BLACK = pygame.Color("Black")
-    WHITE = pygame.Color("White")
-    IMAGE_SIZE = 470
+    black = pygame.Color("Black")
+    white = pygame.Color("White")
+    image_size = 470
     TEXT_HEIGHT = 30
 
     # initialize the pygame module
     pygame.init()
 
     # prepare the window (screen)
-    screen = pygame.display.set_mode((IMAGE_SIZE, IMAGE_SIZE + TEXT_HEIGHT))
+    screen = pygame.display.set_mode((image_size, image_size + TEXT_HEIGHT))
     pygame.display.set_caption("Text, Sound, and an Image")
 
     # Prepare the image
     # TODO 1: Create an image with the 2dogs.JPG image
+    image1 = pygame.image.load("2dogs.JPG")
     # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    image1 = pygame.transform.scale(image1, (image_size, image_size + TEXT_HEIGHT))
 
     # Prepare the text caption(s)
     # TODO 4: Create a font object with a size 28 font.
+    font1 = pygame.font.SysFont("arial", TEXT_HEIGHT)
     # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
-
+    caption1 = font1.render("Two Dogs", True, white)
     # Prepare the music
     # TODO 8: Create a Sound object from the "bark.wav" file.
 
@@ -34,17 +37,23 @@ def main():
             # TODO 9: Play the music (bark) if there's a mouse click.
 
         # Clear the screen and set the screen background
-        screen.fill(WHITE)
+        screen.fill(white)
 
         # Draw the image onto the screen
         # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
+        screen.blit(image1, (0, 0))
 
         # Draw the text onto the screen
         # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
+        screen.blit(caption1, (image1.get_width()//2 - caption1.get_width()//2 - 20,
+                               image1.get_height() - 80))
 
         # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        caption2 = font1.render("MEOW", True, black)
+        screen.blit(caption2, (image1.get_width() // 2 - caption1.get_width() // 2 + 80,
+                               image1.get_height() - 400))
 
         # Update the screen
         pygame.display.update()
